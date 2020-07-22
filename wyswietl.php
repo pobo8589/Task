c9a8c7ef52a4ed7001f7fa1c37be4436da179d0e
Bartosz Poborowski <bartosz.poborowski@amplusfoods.com>
2020-07-22 13:00:37 +0200

    dodalem nowa funkcjonalnosc

--- wyswietl.php
+++ wyswietl.php
@@ -6,11 +6,11 @@ $db="baza";
 $conn=mysqli_connect($host,$user,$pass,$db)or die("blad polaczenia");
 $q="SELECT * FROM `todo` ";
 $wynik=mysqli_query($conn,$q)or die("blad zapytania");
-$id = (int)$_POST['id'];
+//$id = (int)$_POST['id'];
 echo"<ul>";
     while($row=mysqli_fetch_array($wynik)){
 //    echo"<li>".$row['nazwa']."</li>";
-      echo "<li>".$row['id'].': '.$row['nazwa'].''."<a href='usun.php?del=<?php echo '$id'' ?>".'usun'."</a>".''."</li>";
+      echo "<li>".$row['id'].': '.$row['nazwa']."</li>";
 
     }
 echo"</ul>";
@@ -20,3 +20,5 @@ echo"</ul>";
 <!---->
 <!--''."<a href='usun.php?del=--><?php //echo $id ?><!--'".'usun'."</a>".-->
 <!--<a href="usun.php?del=--><?php //echo $id ?><!--">usun</a>-->
+
+<!--    ''."<a href='usun.php?del=--><?php /*echo '$id'' ?>".'usun'."</a>".''.*/
